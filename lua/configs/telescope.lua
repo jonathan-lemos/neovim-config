d9ae5688e213ad6de961f633b5f42967ccbb2961
Jonathan Lemos <jonathanlemos@protonmail.com>
2022-08-29 23:13:41 -0400

    telescope bindings, single esc to quit

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -1,8 +1,18 @@
-local telescope = require 'telescope'
-require 'vider/map'
+return function(setup)
+    local telescope = require 'telescope'
+    local actions = require 'telescope.actions'
+    require 'vider/map'
 
-telescope.setup {}
+    telescope.setup {
+        defaults = {
+            mappings = {
+                i = {
+                    ["<ESC>"] = actions.close,
+                }
+            }
+        }
+    }
 
-noremap { '<Leader>ff', '<cmd> Telescope find_files<CR>' }
-noremap { '<Leader>fg', '<cmd> Telescope live_grep<CR>' }
-noremap { '<Leader>fb', '<cmd> Telescope buffers<CR>' }
+    noremap { setup.keybinds.find_files, '<cmd> Telescope find_files<CR>' }
+    noremap { setup.keybinds.live_grep, '<cmd> Telescope live_grep<CR>' }
+end
